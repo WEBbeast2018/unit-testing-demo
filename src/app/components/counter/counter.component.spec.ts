0b539b45e127b24bfcd551a0439a80e98666b0e6
Roy Nevo <36882185+TheBigR@users.noreply.github.com>
2018-08-01 18:37:57 +0300

    finished 4 tests

--- src/app/components/counter/counter.component.spec.ts
+++ src/app/components/counter/counter.component.spec.ts
@@ -22,4 +22,33 @@ describe('CounterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should increment',() => {
+    const currentCount = component.counter;
+    component.incerement();
+    fixture.detectChanges();
+    expect(component.counter).toBeGreaterThan(currentCount);
+  });
+  it('should decrement',() => {
+    component.incerement();
+    const currentCount = component.counter;
+    component.decrement();
+    fixture.detectChanges();
+    expect(currentCount).toBeGreaterThan(component.counter);
+  });
+  it('validate min rule',() => {
+    const currentCount = component.counter;
+    component.decrement();
+    fixture.detectChanges();
+    expect(currentCount).toBe(component.counter);
+  });
+  it('validate max rule',() => {
+    component.incerement();
+    component.incerement();
+    component.incerement();
+    component.incerement();
+    const currentCount = component.counter;
+    component.incerement();
+    fixture.detectChanges();
+    expect(currentCount).toBe(component.counter);
+  });
 });
